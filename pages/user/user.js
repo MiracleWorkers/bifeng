f402e23be84d68917e2bc83686a5dee2a7c6e4b8
atomjaylee <908105576@qq.com>
2018-05-26 18:33:34 +0800

    feature# userPage

--- pages/user/user.js
+++ pages/user/user.js
@@ -1,11 +1,10 @@
 // pages/user/user.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
   
+  data: {
+    // init：首次进入； provider： 服务商页面； master： 雇主页面
+    userPage: "init"
   },
 
   /**
@@ -15,52 +14,5 @@ Page({
   
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
   
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
 })
